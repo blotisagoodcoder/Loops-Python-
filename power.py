53b7b4c95c674028c7000109ffa88d1541fe58da
blotisagoodcoder <mayleen.amarillo024@gmail.com>
2026-03-26 12:51:44 +0800

    Add files via upload

---
+++ power.py
@@ -0,0 +1,6 @@
+number = int(input("Hello mere mortal! Bestow a number upon me and I shall help you calculate you're way to victory!"))
+
+power = int(input("Why greetings again mortal! The power you enter shall persuade for the answer!"))
+
+result = number ** power
+print("The answer reveals itself! It is", result,"! Have my power say welcome to answering that question of your's mortal! We shall meet again!")
