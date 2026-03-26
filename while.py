9a4e5dd76847cc4bc26bbb760d15d016fbb2dfc3
blotisagoodcoder <mayleen.amarillo024@gmail.com>
2026-03-26 14:18:07 +0800

    Add files via upload

---
+++ while.py
@@ -0,0 +1,7 @@
+g = int(input("Heyha just uh enter the value of terms."))
+sum = 0
+j = 1
+while j<=g:
+    sum = sum+j
+    j=j+1
+    print("\nsum=", sum, "alright, thats all. Bye bye [redacted]")
