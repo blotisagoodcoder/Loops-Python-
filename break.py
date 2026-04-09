ceff515f6c9d0a74de67533e498c0569cdd89f4a
blotisagoodcoder <mayleen.amarillo024@gmail.com>
2026-04-09 14:32:52 +0800

    Add files via upload

---
+++ break.py
@@ -0,0 +1,8 @@
+text = input("ENTER A STRING NOW! OR ELSE!")
+
+for char in text:
+   if char=='A':
+      print("A IS FOUND")
+      break
+   else:
+      print ("A NOT FOUND HEHE")
---
+++ pass.py
@@ -0,0 +1,15 @@
+for x in range(10):
+    if x % 20 == 0:
+        print("TWIIIIIST!")
+
+if x % 15 == 0:
+ pass
+
+if x % 5 == 0:
+ print("FIZZ")
+
+if x % 3 == 0:
+ print("BUZZZZ")
+
+else:
+ print(x)
