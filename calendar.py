b215dc0f0b63f8530204f0889bf0d0fac2d8922b
blotisagoodcoder <mayleen.amarillo024@gmail.com>
2026-04-16 14:39:53 +0800

    Add files via upload

---
+++ calendar.py
@@ -0,0 +1,6 @@
+import calendar
+
+year = 2013
+month = 1
+
+print(calendar.month(year,month))
---
+++ date.py
@@ -0,0 +1,8 @@
+from datetime import date , time , datetime
+today = date.today()
+now = datetime.now()
+print("TODAY'S DATE: ", today)
+print("\nCURRENT DATE AND TIME: ",now)
+
+print("\nDate components","CURRENT YEAR:",today.year,"CURRENT")
+print("MONTH", today.month, "Today's Date",today.day )
